890e90001511cc3dc422d30431eff9a1658bea23
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-23 19:27:56 +0530

    handling

---
+++ catalogue.sh
@@ -0,0 +1,20 @@
+source common.sh
+app_name=catalogue
+
+nodejs_setup
+
+print_heading "Copy MongoDB repo file"
+cp $scripts_path/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
+status_check $?
+
+print_heading "Install MongoDB Client"
+dnf install mongodb-mongosh -y &>>$log_file
+status_check $?
+
+print_heading "Load Master Data"
+mongosh --host mongodb.rdevopsb81.online </app/db/master-data.js  &>>$log_file
+status_check $?
+
+print_heading "Restart Catalogue Service"
+systemctl restart catalogue &>>$log_file
+status_check $?
